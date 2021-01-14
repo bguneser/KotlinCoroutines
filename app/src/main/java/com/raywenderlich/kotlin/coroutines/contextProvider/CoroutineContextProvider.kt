2963cc95f1792fa2d31b11402da6c09025fc393d
bguneser <bguneser@bgunesers-mbp.local>
2021-01-14 23:16:14 +0300

    Kotlin coroutine : Async , coroutine mock - unit test , corouitine log/debug , dependency injection features have been implemented and themoviedb apis are used for data fetching

---
+++ app/src/main/java/com/raywenderlich/kotlin/coroutines/contextProvider/CoroutineContextProvider.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.kotlin.coroutines.contextProvider
+
+import kotlin.coroutines.CoroutineContext
+
+interface CoroutineContextProvider {
+
+    fun context():CoroutineContext
+
+}
